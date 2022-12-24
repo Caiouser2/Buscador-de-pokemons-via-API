c1dd9b3e9855507d189168e8a2ad9b69a1429c98
Caiouser2 <ocaio997@gmail.com>
2022-12-24 16:53:30 -0300

    update layout and optimization

--- script.js
+++ script.js
@@ -6,18 +6,21 @@ const containerErrorsCustoms = document.querySelector('.error-custom'); //contai
 textarea.addEventListener('click', () => {
     containerErrorsCustoms.classList.add('none');
 }) //always that textarea recive a click, the message of erros it is hide
+/******************* section get elements dynamics errors *********************************/
+
+/******************* section get elements of form search *********************************/
+const li = document.querySelectorAll('.all-li'); //li of suggestions
 
 const btn = document.querySelector('.btn');
 btn.addEventListener('click', submitRequest);
-
-let firstArr = []; //first array, recive all pokemons names for dynamic sugestions sugestions   
 
 const ul = document.querySelector('.sugestions');
 document.addEventListener('click', () => {
     ul.classList.add('none');
 });
 
-const li = document.querySelectorAll('.all-li'); //li of suggestions
+/******************* section verify value of input search *********************************/
+let firstArr = []; //first array, recive all pokemons names for dynamic sugestions sugestions   
 
 function shrinkArrayByFiveSpaces() {
     ul.classList.remove('none');
@@ -45,13 +48,13 @@ function shrinkArrayByFiveSpaces() {
             li[n].addEventListener('click', () => {
                 textarea.value = `${sencondArrFiveSpaces}`;
                 ul.classList.add('none');
-            }); //when you click on li it adds value in the textarea
+            }); //when you click on li it add value in the input search
         }   
     }
     changeUlForFiveSpaces();
 }
 
-//make request the names of pokemons in the API
+/************************ section make request the names of pokemons in the API ****************************************/
 function requestNamesPokemons() {
     fetch("https://pokeapi.co/api/v2/pokemon/?limit=899&offset=01")
     .then(res => res.json())
@@ -61,13 +64,12 @@ function requestNamesPokemons() {
             firstArr.push(reciveNames); 
         }        
     })
-    .catch()
+    .catch();
 }
 requestNamesPokemons(); //this function takes the name of the pokemons for dynamics sugestions
 
 
-
-//make request of informations pokemons on API
+/************************* make request about informations pokemons on API ************************************/
 function submitRequest() {
     containerErrorsCustoms.classList.add('none');
 
@@ -83,57 +85,54 @@ function submitRequest() {
     .then(pokemons => {
         const resetPokemonsNameInCard = pokemons.name[0].toUpperCase() + pokemons.name.substr(1); //change first letter of pokemon name for to upper case
 
-        const cardPokemons = containerCardPokemons.innerHTML = `
+        const cardPokemons = innerHTML = `
         <div class="information-pokemon">
-        <h1>${resetPokemonsNameInCard}</h1>
-        <section id="alternative-image">
-        <img src="https://cdn.traction.one/pokedex/pokemon/${pokemons.id}.png">
-        </section>
-        <div class="description">
-        <h2>type:</h2>
-        <h2>${pokemons.types[0].type.name}</h2>
-        <h2 id="second-type-pokemon"></h2>
-        </div>
+            <h1>${resetPokemonsNameInCard}</h1>
+            <section id="alternative-image">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemons.id}.png">
+            </section>
+            <div class="description">
+                <h5>Type: <span>${pokemons.types[0].type.name}</span>, <span>${verifyLengthArrayTypesPokemons()}</span></h5>
+                <h5>abilities: <span>${pokemons.abilities[0].ability.name}</span>, <span>${verifyLengthArrayAbilitiesPokemons()}</span></h5>
+            </div>
         </div>`;
 
-        if (pokemons.id >= 888) imgAlternativePokemon(); //above id 800 images are missing from the PokéDex API, so I get sprites from BASE_URL_POKEMONS
-        if (pokemons.types.length == 2) checkPokemonType(); //this function check if there is more than one type in the array, if so add it to the html
+        containerCardPokemons.innerHTML = cardPokemons; 
 
-        function imgAlternativePokemon() {
-            const divImage = document.querySelector('#alternative-image');
-            const getNewImage = divImage.innerHTML = `<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemons.id}.png">` 
-            return;
-            //above id 800 images are missing from the PokéDex API, so I get sprites from BASE_URL_POKEMONS
-
+        function verifyLengthArrayTypesPokemons() {
+            if (pokemons.types.length > 1) {
+                return ` ${pokemons.types[1].type.name}`;
+            } else {
+                return '';
+            }
         }
 
-        function checkPokemonType() {
-            const description = document.querySelector('#second-type-pokemon');
-            const secondTypepokemon = description.innerHTML = ` ${pokemons.types[1].type.name}`
-            return;
-            //Check if there is more than one type in the array, if so add it to the html
+        function verifyLengthArrayAbilitiesPokemons() {
+            if (pokemons.abilities.length > 1) {
+                return ` ${pokemons.abilities[1].ability.name}`;
+            } else {
+                return '';
+            }
         }
     })
-    .catch(() => {
+    .catch((error) => {
         const elementMessageErrorOne = document.querySelector('.error-custom > .error-pokemon-null');
         containerErrorsCustoms.classList.remove('none');
         elementMessageErrorOne.classList.remove('none');
-
-        containerCardPokemons.classList.add('none')
     }) 
 }
 
-            //pokemon selection session by key//
-
+/******************* section select suggestion of input for key *********************************/
 let numberHoverSugestion = -1; //selector start number 
 let maxSugestions = 4; //maximum selector number 
 
 
 function selectorHoverSugestionUp(keyUp) {
-    if (keyUp.keyCode === 38){
+    if (keyUp.keyCode === 38) {
         numberHoverSugestion--
 
-        if (numberHoverSugestion < 0) numberHoverSugestion = maxSugestions;
+        if (numberHoverSugestion < 0) 
+            numberHoverSugestion = maxSugestions;
 
         for (let n = 0; n <= 4; n++) {
 
@@ -147,6 +146,7 @@ function selectorHoverSugestionUp(keyUp) {
         }
     }    
 }
+
 //this function directs the selector up
 //in the Li there are values from 0 to 4, when numberHoverSugestion is equal to this value it gets the checked class which makes its background gray
 textarea.addEventListener('keyup', selectorHoverSugestionUp)
@@ -155,7 +155,8 @@ function selectorHoverSugestionDown(keyDown) {
     if (keyDown.keyCode === 40) {
         numberHoverSugestion++
 
-         if (numberHoverSugestion > maxSugestions) numberHoverSugestion = 0;
+        if (numberHoverSugestion > maxSugestions)
+            numberHoverSugestion = 0;
 
         for (let n = 0; n <= 4; n++) {
             if (li[n].classList.contains('none') == false) maxSugestions = n;
@@ -168,18 +169,23 @@ function selectorHoverSugestionDown(keyDown) {
         }
     }
 }
+
+
 //this function directs the selector down
 //in the Li there are values from 0 to 4, when numberHoverSugestion is equal to this value it gets the checked class which makes its background gray
 textarea.addEventListener('keyup', selectorHoverSugestionDown);
 
 function selectHoverSugestion(keyEnter) {
     if (keyEnter.keyCode === 13 ) {
-        if(textarea.value == "") numberHoverSugestion = -1;
+        if(textarea.value == "")
+         numberHoverSugestion = -1;
 
         if (numberHoverSugestion === -1) {
             submitRequest() 
             //if numberHoverSugestion === -1 it means that the user did not use the keyboard selector, so the request is made by default
-        } else if (numberHoverSugestion !== -1) {
+        } 
+        
+        if (numberHoverSugestion !== -1) {
             const filterArr = firstArr.filter(oldArr => oldArr.includes(`${textarea.value}`));
             let sencondArr = [];
 
@@ -191,10 +197,10 @@ function selectHoverSugestion(keyEnter) {
                 textarea.value = `${sencondArr[numberHoverSugestion]}`;
 
                 ul.classList.add('none');
+                //if numberHoverSugestion !== -1 means that the user used the selector via keyboard, now the array is handled so that only the name that was clicked on is fetched from the API 
             }
-        submitRequest();
+            submitRequest();
         }
-    //if numberHoverSugestion !== -1 means that the user used the selector via keyboard, now the array is handled so that only the name that was clicked on is fetched from the API 
     }
 }
 document.addEventListener('keyup', selectHoverSugestion);
